69225f0af8b305755b6651b3610d970bc3acbb2a
BILLY HO <billyho0906@gmail.com>
2014-11-13 12:28:57 +0000

    toy_app exercise

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   has_many :microposts
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 } 
+  validates :content, presence: true
 end
